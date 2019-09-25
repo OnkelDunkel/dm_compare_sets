6e7f2deb0c805ef148bf93bffada5bdabf444a86
OnkelDunkel <rasbalnor@gmail.com>
2019-09-25 10:30:47 +0200

    done

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] A = {1,2,3,4};
+        int[] B = {1,2,3,5};
+        System.out.println(compareSets(A,B));
+    }
+    public static int compareSets(int[] A, int[] B) {
+
+        if (A.length < B.length) {
+            boolean allInB = true;
+            for (int i:A) {
+                if (!existsInArr(i, B)) allInB = false;
+            }
+            if (allInB) return -1;
+        }
+        else if (A.length > B.length) {
+            boolean allInA = true;
+            for (int i:B) {
+                if (!existsInArr(i, A)) allInA = false;
+            }
+            if (allInA) return 1;
+        }
+        else {
+            boolean allInA = true;
+            for (int i:B) {
+                if (!existsInArr(i, A)) allInA = false;
+            }
+            if (allInA) return 0;
+        }
+        return -2;
+    }
+    static boolean existsInArr (int number, int[] arr) {
+        for (int i: arr) {
+            if (number == i) return true;
+        }
+        return false;
+    }
+}
